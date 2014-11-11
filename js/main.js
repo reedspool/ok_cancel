b5b08537f9600f8e19f54467a6a3981bb1bb3db9
reedspool <splinterspool@yahoo.com>
2014-11-10 23:27:44 -0500

    Lots of work on editor

--- js/main.js
+++ js/main.js
@@ -1,17 +1,9 @@
 require.config({
 	paths: {
-		game: './game',
-		theWoods: './theWoods'
 	},
     shim: {
 	    underscore: {
 			exports: '_'
 	    }
 	}
-});
-
-require(['game','theWoods'], function (game, theWoods) {
-	game.create(theWoods).run();
-
-	function log(m) { console.log(m) };
 });
